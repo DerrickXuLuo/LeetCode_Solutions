c394ae97abddaabfbdb6a85a2382b20584f8ba65
DerrickXuLuo <43115431+DerrickXuLuo@users.noreply.github.com>
2020-05-05 23:27:38 -0500

    Update 1.TwoSum.java

--- java/1.TwoSum.java
+++ java/1.TwoSum.java
@@ -1,19 +1,25 @@
 class Solution {
+    /**
+     * One pass Hashmap solution: Time Complexity = O(n); Space Complexity = O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
-	    if(nums == null || nums.length == 0){
-            return new int[2];
+        if (nums == null || nums.length == 0){
+            return new int[]{};
         }
-        
+
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++){
             int rem = target - nums[i];
-            if(map.containsKey(rem)){
+            if (map.containsKey(rem)){
                 return new int[]{i, map.get(rem)};
-            }else{
-                map.put(nums[i], i);   
+            }else {
+                map.put(nums[i], i);
             }
         }
-        
-        return new int[2];
+
+        return new int[]{};
     }
 }
