4a2b8d98c39b1ef467d65b1ff9efbb1f572bb356
DerrickXuLuo <43115431+DerrickXuLuo@users.noreply.github.com>
2020-05-09 17:47:18 -0500

    Update LC11.java

--- java/LC11.java
+++ java/LC11.java
@@ -1,4 +1,11 @@
-  public class LC11 {
+public class LC11 {
+    /**
+     * Time Complexity = O(n)
+     * Proved by contradiction:
+     * https://leetcode.com/problems/container-with-most-water/discuss/6089/Anyone-who-has-a-O(N)-algorithm/7268
+     * @param height
+     * @return
+     */
     public int maxArea(int[] height) {
         int maxArea = 0, l = 0, r = height.length - 1;
         while (l < r){
