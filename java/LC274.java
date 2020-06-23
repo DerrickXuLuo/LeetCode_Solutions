289c232885feed14c14dfb1765e22ffd3f94fcbe
DerrickXuLuo <43115431+DerrickXuLuo@users.noreply.github.com>
2020-06-23 11:13:22 -0500

    Update LC274.java

--- java/LC274.java
+++ java/LC274.java
@@ -23,7 +23,7 @@ public class LC274 {
         Arrays.sort(arr, Collections.reverseOrder());
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < i + 1) {
-                return i + 1;
+                return i;
             }
         }
 
