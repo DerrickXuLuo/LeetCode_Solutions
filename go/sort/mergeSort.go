cff44bed09d8795947af5b090b776a0e990d9139
DerrickXuLuo <43115431+DerrickXuLuo@users.noreply.github.com>
2020-11-21 12:42:38 +0800

    Update mergeSort.go

--- go/sort/mergeSort.go
+++ go/sort/mergeSort.go
@@ -1,42 +1,37 @@
 package main
 
-func quickSort(arr []int) []int {
-	return helper(arr, 0, len(arr) - 1)
-}
-
-func helper(arr []int, left int, right int) []int {
+func MergeSort(arr []int) []int {
 	if arr == nil || len(arr) == 0 {
 		return make([]int, 0)
 	} else if len(arr) < 2 {
 		return arr
 	}
 
-	pivotIndex := partition(arr, left, right)
-	if pivotIndex > left {
-		helper(arr, left, pivotIndex - 1)
-	}
-	if pivotIndex < right {
-		helper(arr, pivotIndex + 1, right)
-	}
+	mid := len(arr) / 2
 
-	return arr
+	return merge(MergeSort(arr[:mid]), MergeSort(arr[mid:]))
 }
 
-func partition(arr []int, left int, right int) int {
-	pivot := arr[right]
-	idx := left - 1
-	for i := left; i < right; i++ {
-		if arr[i] <= pivot {
-			idx++
-			swap(arr, i, idx)
+func merge(left []int, right []int) []int {
+	ans := make([]int, len(left) + len(right))
+
+	for k, i, j := 0, 0, 0; k < len(ans); k++ {
+		if i < len(left) && j < len(right) {
+			if left[i] < right[j] {
+				ans[k] = left[i]
+				i++
+			} else {
+				ans[k] = right[j]
+				j++
+			}
+		} else if i >= len(left) {
+			ans[k] = right[j]
+			j++
+		} else {
+			ans[k] = left[i]
+			i++
 		}
 	}
-	swap(arr, idx + 1, right)
-	return idx + 1
-}
 
-func swap(arr []int, i int, j int) {
-	tmp := arr[i]
-	arr[i] = arr[j]
-	arr[j] = tmp
+	return ans
 }
